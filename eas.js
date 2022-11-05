dfa8278ef9579031ba430b9f41e06c8a9a8ca2c3
cgunn92 <nominetrinitatis@gmail.com>
2022-11-04 18:30:47 -0700

    Add style to boxes on hover

--- eas.js
+++ eas.js
@@ -1,9 +1,37 @@
 const content = document.querySelector('.content');
 const button = document.querySelector('button');
 
-for(let i = 0; i < 25; i++){
+button.addEventListener('click', function(e){
+    let boxes = parseInt(prompt("Enter a number between 1 and 100: "));
+    console.log('boxes: ' + boxes);
+    if(boxes === NaN || boxes > 100 || boxes < 1) { return; };
+
+    boxesSqrd = boxes * boxes;
+    console.log('boxes sqrd: ' + boxesSqrd);
+    createBoxes(boxesSqrd, boxes);
+})
+
+function createBoxes(boxesSqrd, numberOfBoxes){
+    
+    for(let i = 0; i < boxesSqrd; i++){
+        
+        const box = document.createElement('div');
+        box.className = 'box';
+        box.style.height = `calc(1000px / ${numberOfBoxes})`;
+        box.style.width = `calc(1000px / ${numberOfBoxes})`;
+        content.appendChild(box);
+
+        box.addEventListener('mouseover', function(e){
+            box.style.backgroundColor = 'black';
+        });
+    }
+
+}
+
+
+/*for(let i = 0; i < 25; i++){
     const box = document.createElement('div');
     box.className = 'box';
     box.textContent = 'Im a box';
     content.appendChild(box);
-}
+}*/
